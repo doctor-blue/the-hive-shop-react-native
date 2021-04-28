fbd6409c0bae2d9ddaf201382dbefe609cc0bb02
Nguyen Van Tan <doctorblue.king@gmail.com>
2021-04-28 10:29:26 +0700

    Init project

--- ios/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  the_hive_shop
-//
-//  Created by Nguyen Van Tan on 27/04/2021.
-//
-
-import Foundation
---
+++ ios/Pods/Target Support Files/Pods-the_hive_shop-the_hive_shopTests/Pods-the_hive_shop-the_hive_shopTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_the_hive_shop_the_hive_shopTests : NSObject
+@end
+@implementation PodsDummy_Pods_the_hive_shop_the_hive_shopTests
+@end
